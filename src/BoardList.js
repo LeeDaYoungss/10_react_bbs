4a8d3c03b621c9fa757819e51df6a800707fccf7
LeeDaYoungss <leedayeonggg@gmail.com>
2024-06-10 11:47:37 +0900

    first commit

---
+++ src/BoardList.js
@@ -0,0 +1,300 @@
+import React, { Component, useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Axios from 'axios';
+import Board from './Board';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+
+
+// 함수형
+
+/* 수정 프로세스 정리 요약
+
+boardList 숫자 체크 수정
+hamdleModify, id 전달
+
+App.js
+모드수정
+boardId 반영(변경)
+👇🏻
+Write.js
+handleCancle 실행
+👇🏻
+App.js
+handleCancle 실행 -> isComplete (false)
+👇🏻
+BoardList.js
+isComplete (false) 로 넘어옴
+getList() 실행
+renderComplete 함수 부모 전달
+👇🏻
+App.js
+renderComplete -> setComplete
+*/
+
+function BoardList(props){
+  const [boardList, setBoardList] = useState([]);
+  const [checkList, setCheckList] = useState([]);
+  const [boardId, setBoardId] = useState(0);
+  const navigate = useNavigate();
+
+  let getList = () => {
+    Axios.get('http://34.64.171.168:8000/list')
+    .then(res => {
+      const data = res.data;
+      setBoardList(data);
+      setCheckList([]);
+      setBoardId(0);
+      props.renderComplete(); // 목록 출력 완료
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+  useEffect(()=>{
+    getList(); 
+  },[]) // 최초 한번 실행, 결과가 나오면 실행
+
+  let onCheckboxChange = (checked, id) => {
+    let list = checkList;
+    if (checked) {
+      if (list.indexOf(id) === -1) {
+        list.push(id);
+      }
+    } else {
+      let idx = list.indexOf(id);
+      list.splice(idx, 1);
+    }
+    setCheckList(list);
+  };
+  let handleModify = () => {
+    let checklist = checkList;
+    if (checklist.length === 0) {
+      alert('최소 하나를 체크해주세요');
+    } else if (checklist.length > 1) {
+      alert('하나만 체크해주세요');
+    } else {
+      setBoardId(checklist[0]); 
+      props.handleModify(checklist[0]);
+      navigate("/write");
+    }    
+  };
+  let handleDelete = () => {
+    //체크 항목이 없으면 '삭제할 게시글을 선택하세요' 경고창 
+    if(checkList.length === 0){
+      alert('삭제할 게시글을 선택하세요');
+    }
+    let boardIdList = '';;
+    checkList.forEach(item=>{
+      boardIdList += `'${item}',`;
+    });
+    
+    Axios.post('http://34.64.171.168:8000/delete',{
+      boardIdList : boardIdList.substring(0, boardIdList.length -1)
+     })
+    .then( res => {     
+      getList();
+    })
+    .catch(function (error) {     
+      console.log(error);
+    });
+  };
+  useEffect(()=>{
+    if(!props.isComplete){
+      getList();
+    }
+  },[props.isComplete])
+  return (
+    <>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">선택</th>
+            <th scope="col">번호</th>
+            <th scope="col">제목</th>
+            <th scope="col">작성자</th>
+            <th scope="col">작성일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            boardList.map(item => (
+              <Board key={item.id} data={item} onCheckboxChange={onCheckboxChange} />
+            ))
+          }
+        </tbody>
+      </table>
+      <div className="d-flex justify-content-between gap-3">
+        <div className="d-flex gap-1">
+        
+          <Button variant="info" onClick={handleModify}>수정</Button>
+        
+          <Button variant="danger"  onClick={handleDelete}>삭제</Button>
+        </div>
+        <Link to="/write">
+          <Button variant="primary">글쓰기</Button>
+        </Link>          
+      </div>
+    </>
+  );
+}
+
+export default BoardList;
+
+
+/* class형
+export default class BoardList extends Component {
+  state = {
+    boardList : [],
+    checkList : [],
+    boardId : 0
+  };
+
+  getList = ()=>{
+    Axios.get('http://localhost:8000/list')
+  .then( res=> {
+    const {data} = res; //같은 문장 -> const data = res.data;
+    this.setState({
+      boardList:data,
+      checkList: [],
+      boardId:0
+    })
+    this.props.renderComplete(); //목록 출력 완료
+  })
+  .catch(function (error) {
+    // 에러 핸들링
+    console.log(error);
+  })
+  }
+  componentDidMount(){
+    this.getList(); //최초 한번 작동, 서버의 결과가 나오면 한번 더 실행
+  }
+  onCheckboxChange = (checked, id)=>{
+
+    //기존배열에서 중복제거, 새값이면 추가 (filter method w3schools 참조)
+    let list = this.state.checkList.filter(v=>{
+      return v !== Number(id);
+    });
+
+    // checked가 true일 때만 list 안에 id(번호 1, 2, 3)를 밀어넣음
+    if(checked){
+      if(list.indexOf(id) === -1){
+        list.push(id);
+      }
+    } else {
+      // id 2, false, index 번호를 찾아서(indexOf) splice 해줌
+      let idx = list.indexOf(id);
+      list.splice(idx, 1);
+    }
+    this.setState({
+      checkList:list
+    });
+  }
+
+  handleModify = ()=>{
+    let checklist = this.state.checkList;
+    if(checklist.length === 0){
+      alert('최소 하나를 체크해주세요');
+    }else if(checklist.length > 1){
+      alert('하나만 체크해주세요');
+    }
+    this.setState({
+      boardId : checklist[0]
+    });
+
+    this.props.handleModify(checklist[0]);
+  };
+
+  handleDelete = ()=>{
+    if(this.state.checkList.length === 0){
+      alert('삭제할 게시글을 선택하세요');
+    }
+    let boardIdList = '';
+    // let boardIdList = this.state.checkList.join();
+    this.state.checkList.forEach(item=>{
+      boardIdList += `'${item}',`;
+    })
+    console.log(boardIdList);
+
+    Axios.post('http://localhost:8000/delete',{
+
+      // 마지막 번호에서 쉼표를 빼주기
+      boardIdList : boardIdList.substring(0, boardIdList.length -1)
+    })
+    .then(res => {
+      this.getList();
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  };
+
+  componentDidUpdate = (prevProps, prevState)=>{
+    if(this.state.checkList.length === 1 && this.state.boardId !== prevState.boardId){
+      this.props.handleModify(this.state.boardId);
+    }
+
+    if(!this.props.isComplete && prevProps.isComplete !== this.props.isComplete){
+      this.getList(); //false일때만 다시 불러옴 
+    }
+  }
+  resetCheckList = () =>{
+    this.setState({
+      checkList:[]
+    })
+  }
+
+  render() {
+    console.log(this.state.boardList);
+    return (
+      <>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">선택</th>
+              <th scope="col">번호</th>
+              <th scope="col">제목</th>
+              <th scope="col">작성자</th>
+              <th scope="col">작성일</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              // map 반복문 return(<tr> 추가)
+              this.state.boardList.map(item=>{
+                return(
+                  // Board 컴포넌트 불러오기
+                  <Board key={item.id} data={item} onCheckboxChange={this.onCheckboxChange}/>
+                  // onCheckboxChange -> 할일은 넘어온 값이 true/false 인지 구분해줌
+                )}
+              )
+            }
+          </tbody>
+        </table>
+        <div className="d-flex justify-content-between gap-3">
+          <div className="d-flex gap-1">
+
+              <Button variant="info" onClick={()=>{
+                수정
+                체크하지 않고 수정을 누르면 '최소 하나를 체크해주세요'
+                두개 이상 체크하고 수정을 누르면 '하나만 체크해주세요'
+                
+                this.handleModify();
+              }}>수정</Button>
+
+            <Button variant="danger" onClick={this.handleDelete}>삭제</Button>
+          </div>
+
+          Link 함수로 write 페이지로 이동
+          <Link to="/write"> 
+            <Button variant="primary">글쓰기</Button>
+          </Link>
+          
+        </div>
+      </>
+      
+    )
+  }
+}
+*/
---
+++ src/Write.js
@@ -0,0 +1,250 @@
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+// 함수형
+function Write({boardId, handleCancle, isModifyMode}){
+  const[form, setForm] = useState({
+    title: '', 
+    content:''
+  });
+  
+
+  const navigate = useNavigate();
+
+  let write = ()=>{
+    Axios.post('http://34.64.171.168:8000/insert',{ //post방식으로 넘김
+      title: form.title,
+      content: form.content
+    })
+    .then( res=> {
+      alert('등록 완료');
+      navigate("/");
+    })
+    
+    .catch(function (error) {
+      // 에러 핸들링
+      console.log(error);
+    })
+  }
+
+  let update = ()=>{
+    Axios.post('http://34.64.171.168:8000/update',{ //post방식으로 넘김
+      // id, title, content 세개가 넘어옴
+      id:boardId,
+      title: form.title,
+      content: form.content
+    })
+    .then( res => {
+      alert('수정 완료');
+      navigate("/");
+  
+      // 초기화 - 수정 성공하면 할 일
+      setForm({
+        title: '', 
+        content:''
+      });
+      handleCancle();
+    })
+    
+    .catch(function (error) {
+      // 에러 핸들링
+      console.log(error);
+    })
+    }
+
+  let detail = ()=>{
+    Axios.get(`http://34.64.171.168:8000/detail?id=${boardId}`)
+      .then(res=> {
+        if(res.data.length > 0){
+          setForm({
+            title:res.data[0].title,
+            content:res.data[0].content,
+          })
+        }
+      })
+
+    .catch(function (error) {
+      // 에러 핸들링
+      console.log(error);
+    })
+  }
+  useEffect(()=>{
+    if(isModifyMode && boardId){
+      detail();
+    }
+  },[isModifyMode, boardId])
+
+  let inputHandler = (e)=>{
+
+      if(e.target === 'title'){
+        setForm({...form, title:e.target.value})
+      }else{
+        setForm({...form, content:e.target.value})
+      }
+    
+  }
+
+  return (
+    <>
+      <Form>
+        <Form.Group className="mb-3" controlId="title">
+          <Form.Label>제목</Form.Label>
+          <Form.Control type="text"
+                        name="title"
+                        placeholder="제목을 입력하세요"
+                        onChange={inputHandler}
+                        value={form.title}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="content">
+          <Form.Label>내용</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            name="content"
+            onChange={inputHandler}
+            value={form.content}
+          />
+        </Form.Group>
+      </Form>
+      <div className="d-flex gap-1">
+        <Button variant="info" onClick={isModifyMode ? update : write}>작성완료</Button>
+
+        <Link to="/"> 
+          <Button variant="secondary">취소</Button>
+        </Link>
+        
+      </div>
+    </>
+  )
+
+}
+
+export default Write;
+
+/* class형
+export default class Write extends Component {
+  state = {
+    title: '', //수정모드일 때 제목이 나오기 때문에 변수를 만들어줌
+    content:'' //수정모드일 때 내용이 나오기 때문에 변수를 만들어줌
+  }
+
+  write = ()=>{
+    Axios.post('http://localhost:8000/insert',{ //post방식으로 넘김
+      title: this.state.title,
+      content: this.state.content
+    })
+    .then( res=> {
+      alert('등록 완료');
+    })
+    
+    .catch(function (error) {
+      // 에러 핸들링
+      console.log(error);
+    })
+  }
+
+  // 수정완료를 누르면 작동
+  update = ()=>{
+    Axios.post('http://localhost:8000/update',{ //post방식으로 넘김
+      // id, title, content 세개가 넘어옴
+      id:this.props.boardId,
+      title: this.state.title,
+      content: this.state.content
+    })
+  .then( res => {
+    alert('수정 완료');
+
+    // 초기화 - 수정 성공하면 할 일
+    this.setState({
+      title: '',
+      content:''
+    });
+    this.props.handleCancle();
+  })
+  
+  .catch(function (error) {
+    // 에러 핸들링
+    console.log(error);
+  })
+  }
+
+  // 수정모드이면서 boardId가 있을때 작동
+  // 번호의 글을 알려줄래? get방식으로!
+  detail = ()=>{
+    Axios.get(`http://localhost:8000/detail?id=${this.props.boardId}`)
+      .then(res=> {
+        if(res.data.length > 0){
+          this.setState({
+            title:res.data[0].title,
+            content:res.data[0].content,
+          })
+        }
+      })
+
+.catch(function (error) {
+  // 에러 핸들링
+  console.log(error);
+})
+  }
+
+  componentDidUpdate = (prevProps)=>{
+    console.log('componentDidUpdate');
+    // this.props.boarId(=> 아이디가 있는지 확인)
+    if(this.props.isModifyMode && this.props.boardId !== prevProps.boardId){
+      this.detail();
+    }
+  }
+  // 수정 내용 표시
+  componentDidMount = ()=>{
+    this.detail();
+  }
+
+  inputHandler = (e)=>{
+    this.setState(
+      {[e.target.name]:e.target.value}
+    )
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <>
+        <Form>
+          <Form.Group className="mb-3" controlId="title">
+            <Form.Label>제목</Form.Label>
+            <Form.Control type="text"
+                          name="title"
+                          placeholder="제목을 입력하세요"
+                          onChange={this.inputHandler}
+                          value={this.state.title}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="content">
+            <Form.Label>내용</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              name="content"
+              onChange={this.inputHandler}
+              value={this.state.content}
+            />
+          </Form.Group>
+        </Form>
+        <div className="d-flex gap-1">
+          ajax에선 3안연산자 사용 
+          <Button variant="info" onClick={this.props.isModifyMode ? this.update : this.write}>작성완료</Button>
+
+           Link 함수로 다시 리스트 페이지로 이동 
+          <Link to="/"> 
+            <Button variant="secondary">취소</Button>
+          </Link>
+          
+        </div>
+      </>
+    )
+  }
+}*/
